c1f75a08d8e3c2041309e890c09cc988b04bd15b
Sehara1022 <seharabanu.mulla@coderview.io>
2024-06-28 11:18:24 +0530

    formatted main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -42,9 +42,9 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_instance" "web" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.main.id
+  ami             = var.ami_id
+  instance_type   = var.instance_type
+  subnet_id       = aws_subnet.main.id
   security_groups = [aws_security_group.main.name]
 
   tags = {
