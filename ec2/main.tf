51d6e06bf7644fe88a0855b74119aa02aa3795a3
Sehara1022 <seharabanu.mulla@coderview.io>
2024-06-28 10:16:30 +0530

    adding infracost files

---
+++ ec2/main.tf
@@ -0,0 +1,65 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.subnet_cidr
+  availability_zone = var.aws_availability_zone
+}
+
+resource "aws_security_group" "main" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_http"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.main.id
+  security_groups = [aws_security_group.main.name]
+
+  tags = {
+    Name = "web-server"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.web.id
+}
+
+output "instance_public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "instance_public_dns" {
+  value = aws_instance.web.public_dns
+}
